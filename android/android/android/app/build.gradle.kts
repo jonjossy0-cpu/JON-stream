905e4839993bc9b5ea014054bc15438d3bacdfef
jonjossy0-cpu <jonjossy0@gmail.com>
2026-09-20 15:10:34 +0300

    Add build.gradle.kts for Android app configuration

---
+++ android/android/android/app/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.jonstream.app"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.jonstream.app"
+        minSdk = 27
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+}
